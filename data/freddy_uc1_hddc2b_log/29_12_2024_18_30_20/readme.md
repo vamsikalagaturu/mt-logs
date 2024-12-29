51d76b392947b48f135458a07fc1aaaef0c2c366
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2024-12-29 18:31:21 +0100

    initially out of limits

---
+++ data/freddy_uc1_hddc2b_log/29_12_2024_18_30_20/readme.md
@@ -0,0 +1,4 @@
+# Run Description - 29_12_2024_18_30_20
+
+- bilateral dist cons - side - 61
+
--- uc_plotter.py
+++ uc_plotter.py
@@ -381,7 +381,7 @@ class UCPlotter:
 
         ### hddc2b
 
-        run_id = "29_12_2024_18_20_50"
+        run_id = "29_12_2024_18_30_20"
 
         plotter = Plotter(self.run_dir)
         plotter.load_data(run_id)
